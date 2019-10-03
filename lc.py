5440062e2b028c75e71a59d7b606d628903b0a5e
Matthew Pontefract <matthew@rethought-solutions.com>
2019-10-03 23:08:03 +0100

    Multiple configuration support

--- lc.py
+++ lc.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import optfn
+import sys
 
 from looper import *
 import settings
@@ -30,8 +31,30 @@ def run_controller(host, port, setup, nloops, channels_per_loop):
     handler.start()
 
 
+def set_config(config):
+    """
+    From settings.CONFIG_SETS pull the config referenced by `config` and copy
+    keys into the main space.
+    """
+    try:
+        cfg = settings.CONFIG_SETS[config]
+    except KeyError:
+        print("Config {} is not defined in settings".format(config))
+        sys.exit(1)
+
+    for k, v in cfg.items():
+        setattr(settings, k, v)
+
+
+def show_configs():
+    print("\nAvailable configurations are:\n")
+    for name, config in settings.CONFIG_SETS.items():
+        print("{}: \t{}".format(name, config['doc']))
+    print()
+
+
 def cli_handler(host="localhost", port=9951, stereo=False,
-                loops=2, nosetup=False):
+                loops=2, nosetup=False, config="default", show_config=False):
     """
     Use:
 
@@ -42,13 +65,20 @@ def cli_handler(host="localhost", port=9951, stereo=False,
     --loops=<n>              - Number of loops to start (default: 2)
     --stero                  - Set if stereo is to be selected
     --nosetup                - Do not re-configure the looper
+    --config=<config>        - Config set defined in settings (default: default)
+    --show_config            - Show configurations available
     --help -h                - Show this help
     """
+
+    if show_config is True:
+        show_configs()
+        sys.exit(0)
 
     # ensure type is correct here
     port = int(port)
     loops = int(loops)
     setup = not nosetup
+    set_config(config)
     run_controller(host, port, setup, loops, 2 if stereo else 1)
 
 
--- settings.py
+++ settings.py
@@ -1,15 +1,46 @@
 DEBUG=False
 
-# For keyboard input RECEIVED over UDP bridge
-#COMMAND_SET = "commands.KeyCommandSet"
-#INPUT_HANDLER = "kbd.InputHandler"
-COMMAND_SET = "midi.MidiCommandSet"
-INPUT_HANDLER = "bridge.BridgeInputHandler"
-
-
 # For the pedal board
 #COMMAND_SET = "commands.StompCommandSet"
 #INPUT_HANDLER = "rpi.InputHandler"
+
+CONFIG_SETS = {
+    "default": {
+        "doc": "Keyboard and OSC looper controller",
+        "COMMAND_SET": "commands.KeyCommandSet",
+        "INPUT_HANDLER": "kbd.InputHandler"
+        },
+    "stomp_midi_server": {
+        "doc": "JACK-side bridge with MIDI command emitter",
+        "COMMAND_SET": "midi.MidiCommandSet",
+        "INPUT_HANDLER": "bridge.BridgeInputHandler"
+        },
+    "stomp_server": {
+        "doc": "JACK-side bridge with stomp box OSC looper controller",
+        "COMMAND_SET": "commands.StompCommandSet",
+        "INPUT_HANDLER": "bridge.BridgeInputHandler"
+        },
+    "key_server": {
+        "doc": "JACK-side bridge with keyboard OSC looper controller",
+        "COMMAND_SET": "commands.KeyCommandSet",
+        "INPUT_HANDLER": "bridge.BridgeInputHandler"
+        },
+    "stomp_pedal_client": {
+        "doc": "Stomp box input bridged to server",
+        "COMMAND_SET": "bridge.BridgeCommandSet",
+        "INPUT_HANDLER": "rpi.InputHandler",
+        },
+    "key_client": {
+        "doc": "Keybord input bridged to server",
+        "COMMAND_SET": "bridge.BridgeCommandSet",
+        "INPUT_HANDLER": "kbd.InputHandler"
+        },
+    "stomp": {
+        "doc": "Stomp box input with OSC looper controller",
+        "COMMAND_SET": "commands.StompCommandSet",
+        "INPUT_HANDLER": "rpi.InputHandler"
+        },
+}
 
 # RPi switch settings
 # list of GPIO inputs to map to switches 0..n
